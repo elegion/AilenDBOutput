54982c3f353b6f761b045525dfc770e2b0990030
Arkady Smirnov <arkadysmirnov@outlook.com>
2018-05-21 16:37:15 +0300

    added empty project

---
+++ AilenDBOutput/AilenDBOutput/AilenDBOutput.h
@@ -0,0 +1,19 @@
+//
+//  AilenDBOutput.h
+//  AilenDBOutput
+//
+//  Created by Arkady Smirnov on 5/21/18.
+//  Copyright © 2018 e-Legion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AilenDBOutput.
+FOUNDATION_EXPORT double AilenDBOutputVersionNumber;
+
+//! Project version string for AilenDBOutput.
+FOUNDATION_EXPORT const unsigned char AilenDBOutputVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AilenDBOutput/PublicHeader.h>
+
+
